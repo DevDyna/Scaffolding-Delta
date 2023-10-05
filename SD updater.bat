1655826be44a9cdce88dd2ecf19f47c6e59071db
DevDyna <andrearisorti2003@gmail.com>
2023-10-05 14:20:58 +0200

    added SD updater
    update version automation

--- SD updater.bat
+++ SD updater.bat
@@ -1,5 +1,6 @@
 @echo off
-type pack.mcmeta
+type pack_old.mcmeta
+echo version ----------------^^
 goto RUN
 
 :ERROR_VERS
@@ -8,20 +9,30 @@ goto RUN
 
 :RUN
 set /p version="Versione pack >"
+TIMEOUT /NOBREAK /T 1 > nul
 echo(%version%|findstr "^[-][1-9][0-9]*$ ^[1-9][0-9]*$ ^0$">nul&&goto EXECUTE||goto ERROR_VERS
 
 
 :EXECUTE
+
+::debug file
+set desc=DEBUG FILE VERSION
+echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack_old.mcmeta
+
 
 ::fancy package
 set desc=Scaffolding Delta Package
 cd package/fancy
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
 cd ..
+echo ^> package-fancy
+TIMEOUT /NOBREAK /T 0 > nul
 ::fast package
 cd fast
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
 cd ..
+echo ^> package-fast
+TIMEOUT /NOBREAK /T 0 > nul
 
 ::CTM module
 cd ..
@@ -29,21 +40,34 @@ cd CTM
 set desc=Scaffolding Delta Module
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
 cd ..
+echo ^> ctm-module
+TIMEOUT /NOBREAK /T 0 > nul
 
 ::distance fancy
 cd distance/fancy
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
 cd ..
+echo ^> distance-fancy-module
+TIMEOUT /NOBREAK /T 0 > nul
 ::distance fast
 cd fast
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
 cd ..
+echo ^> distance-fast-module
+TIMEOUT /NOBREAK /T 0 > nul
 
 ::emit fancy
 cd ..
 cd emit/fancy
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
 cd ..
+echo ^> emit-fancy-module
+TIMEOUT /NOBREAK /T 0 > nul
 ::emit fast
 cd fast
 echo {"pack": {"pack_format":%version%,"description": "%desc%"}} > pack.mcmeta
+echo ^> emit-fast-module
+TIMEOUT /NOBREAK /T 0 > nul
+echo:
+echo Updated all files to %version%
+TIMEOUT /NOBREAK /T 1 > nul
