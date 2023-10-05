706e3d4fc5fa4ba8080cde4be8c111fff886de73
DevDyna <andrearisorti2003@gmail.com>
2023-10-05 14:18:45 +0200

    Update SD packager.bat
    added log info
    removed useless folder motion

--- SD packager.bat
+++ SD packager.bat
@@ -1,96 +1,139 @@
-::@echo off
+@echo off
+echo #creating directories
+TIMEOUT /NOBREAK /T 1 > nul
+echo ^> ZIP\temp
 ::create directory
-mkdir SD\Pack-fast
-mkdir SD\Pack-fancy
+mkdir ZIP\temp\Pack-fast
+echo ^> ZIP\temp\Pack-fast
+
+mkdir ZIP\temp\Pack-fancy
+echo ^> ZIP\temp\Pack-fancy
 
 ::modules
-mkdir SD\CTM
-mkdir SD\emit-fast
-mkdir SD\emit-fancy
-mkdir SD\distance-fast
-mkdir SD\distance-fancy
+mkdir ZIP\temp\CTM
+echo ^> ZIP\temp\CTM
+TIMEOUT /NOBREAK /T 0 > nul
+mkdir ZIP\temp\emit-fast
+echo ^> ZIP\temp\emit-fast
+TIMEOUT /NOBREAK /T 0 > nul
+mkdir ZIP\temp\emit-fancy
+echo ^> ZIP\temp\emit-fancy
+TIMEOUT /NOBREAK /T 0 > nul
+mkdir ZIP\temp\distance-fast
+echo ^> ZIP\temp\distance-fast
+TIMEOUT /NOBREAK /T 0 > nul
+mkdir ZIP\temp\distance-fancy
+echo ^> ZIP\temp\distance-fancy
+TIMEOUT /NOBREAK /T 0 > nul
+echo #robocopy files on directories
+TIMEOUT /NOBREAK /T 1 > nul
 
-::copy files to SD/Pack-fast
+::copy files to ZIP/Pack-fast
 ::from package
-robocopy /E package\fast SD\Pack-fast
-
+robocopy /E package\fast ZIP\temp\Pack-fast > nul
 ::from modules
-robocopy /E emit\fast\assets SD\Pack-fast\assets
-robocopy /E distance\fast\assets SD\Pack-fast\assets
+robocopy /E emit\fast\assets ZIP\temp\Pack-fast\assets > nul
+robocopy /E distance\fast\assets ZIP\temp\Pack-fast\assets > nul
+echo ^> Pack-fast
 
-::copy files to SD/Pack-fancy
+::copy files to ZIP/Pack-fancy
 ::from package
-robocopy /E package\fancy SD\Pack-fancy
-
+robocopy /E package\fancy ZIP\temp\Pack-fancy > nul
 ::from modules
-robocopy /E ctm\assets SD\CTM\assets
-robocopy /E distance\fancy\assets SD\Pack-fancy\assets
-robocopy /E emit\fancy\assets SD\Pack-fancy\assets
+robocopy /E ctm\assets ZIP\temp\CTM\assets > nul
+robocopy /E distance\fancy\assets ZIP\temp\Pack-fancy\assets > nul
+robocopy /E emit\fancy\assets ZIP\temp\Pack-fancy\assets > nul
+echo ^> Pack-fancy
 
-::copy files to SD/CTM
-robocopy /E ctm SD\CTM
+::copy files to ZIP/CTM
+robocopy /E ctm ZIP\temp\CTM > nul
+echo ^> CTM-module
 
-::copy files to SD/distance-fast
-robocopy /E distance\fast SD\distance-fast
-::copy files to SD/distance-fancy
-robocopy /E distance\fancy SD\distance-fancy
+::copy files to ZIP/distance-fast
+robocopy /E distance\fast ZIP\temp\distance-fast > nul
+echo ^> distance-fast-module
 
-::copy files to SD/emit-fancy
-robocopy /E emit\fast SD\emit-fast
-::copy files to SD/emit-fast
-robocopy /E emit\fancy SD\emit-fancy
+::copy files to ZIP/distance-fancy
+robocopy /E distance\fancy ZIP\temp\distance-fancy > nul
+echo ^> distance-fancy-module
 
-mkdir zip
-cd SD
+::copy files to ZIP/emit-fancy
+robocopy /E emit\fast ZIP\temp\emit-fast > nul
+echo ^> emit-fast-module
 
+::copy files to ZIP/emit-fast
+robocopy /E emit\fancy ZIP\temp\emit-fancy > nul
+echo ^> emit-fancy-module
+
+TIMEOUT /NOBREAK /T 1 > nul
+echo #compressing resource to .zip files and move out TEMP folder
+cd ZIP/temp
+::####compression on zip####
+
+::CTM zip and move out
 cd CTM
 tar.exe -cf CTM.zip assets pack.png pack.mcmeta
-robocopy . .. CTM.zip
+robocopy . .. CTM.zip /MOVE > nul
 cd ..
-robocopy . .. CTM.zip /MOVE
+robocopy . .. CTM.zip /MOVE > nul
 
+echo ^> CTM-module
+
+:: distance-fancy zip and move out
 cd distance-fancy
 tar.exe -cf distance-fancy.zip assets pack.png pack.mcmeta
-robocopy . .. distance-fancy.zip
+robocopy . .. distance-fancy.zip /MOVE > nul
 cd ..
-robocopy . .. distance-fancy.zip /MOVE
+robocopy . .. distance-fancy.zip /MOVE > nul
 
+echo ^> distance-fancy-module
+
+:: distance-fast zip and move out
 cd distance-fast
 tar.exe -cf distance-fast.zip assets pack.png pack.mcmeta
-robocopy . .. distance-fast.zip
+robocopy . .. distance-fast.zip /MOVE > nul
 cd ..
-robocopy . .. distance-fast.zip /MOVE
+robocopy . .. distance-fast.zip /MOVE > nul
 
+echo ^> distance-fast-module
+
+:: emit-fancy zip and move out
 cd emit-fancy
 tar.exe -cf emit-fancy.zip assets pack.png pack.mcmeta
-robocopy . .. emit-fancy.zip
+robocopy . .. emit-fancy.zip /MOVE > nul
 cd ..
-robocopy . .. emit-fancy.zip /MOVE
+robocopy . .. emit-fancy.zip /MOVE > nul
 
+echo ^> emit-fancy-module
+
+:: emit-fast zip and move out
 cd emit-fast
 tar.exe -cf emit-fast.zip assets pack.png pack.mcmeta
-robocopy . .. emit-fast.zip
+robocopy . .. emit-fast.zip /MOVE > nul
 cd ..
-robocopy . .. emit-fast.zip /MOVE
+robocopy . .. emit-fast.zip /MOVE > nul
 
+echo ^> emit-fast-module
+
+:: Pack-fancy zip and move out
 cd Pack-fancy
 tar.exe -cf Pack-fancy.zip assets pack.png pack.mcmeta
-robocopy . .. Pack-fancy.zip
+robocopy . .. Pack-fancy.zip /MOVE > nul
 cd ..
-robocopy . .. Pack-fancy.zip /MOVE
+robocopy . .. Pack-fancy.zip /MOVE > nul
 
+echo ^> Pack-fancy
+
+:: Pack-fast zip and move out
 cd Pack-fast
 tar.exe -cf Pack-fast.zip assets pack.png pack.mcmeta
-robocopy . .. Pack-fast.zip
+robocopy . .. Pack-fast.zip /MOVE > nul
 cd ..
-robocopy . .. Pack-fast.zip /MOVE
+robocopy . .. Pack-fast.zip /MOVE > nul
 
+echo ^> Pack-fast
+echo #removing zip/temp directory
+TIMEOUT /NOBREAK /T 1 > nul
+::rem temp packs
 cd ..
-robocopy . ZIP CTM.zip /MOVE
-robocopy . ZIP distance-fancy.zip /MOVE
-robocopy . ZIP distance-fast.zip /MOVE
-robocopy . ZIP emit-fancy.zip /MOVE
-robocopy . ZIP emit-fast.zip /MOVE
-robocopy . ZIP Pack-fancy.zip /MOVE
-robocopy . ZIP Pack-fast.zip /MOVE
-rmdir /s /q SD
+rmdir /s /q temp
